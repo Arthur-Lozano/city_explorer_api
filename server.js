2f2846caa27ecb8f1f6f6df5d76ca52a9dcfb17c
Arthur Lozano <chillout885@gmail.com>
2021-01-12 20:44:34 -0800

    updated movies

--- server.js
+++ server.js
@@ -26,7 +26,8 @@ app.use(cors());
 app.get('/', homeHandler);
 app.get('/location', locationHandler);
 app.get('/movies', moviesHandler);
-app.get('/yelp', yelpHandler);
+// app.get('/yelp', yelpHandler);
+app.get('/weather', weatherHandler);
 app.use('*', notFoundHandler);
 
 function homeHandler(request, response) {
@@ -37,43 +38,28 @@ function homeHandler(request, response) {
 //Movie API
 function moviesHandler(request, response) {//BUILD OUR REQUEST TO TALK TO MOVIEAPI
   //Build request before we send it off to
-  let city = request.query.city;
+  let city = request.query.search_query;
   let key = process.env.MOVIE_API_KEY;
-  const url = `https://api.themoviedb.org/3/movie/550?api_key=${key}`;
-  console.log('Movie api', url);
-  let SQL = 'SELECT * FROM cityexplorer WHERE search_query = $1';
-  let movieQuery = [city];
-  client.query(SQL, movieQuery)
-    .then(results => { //We get our data from the API
-      if (results.rowCount) {// how we know if it is in the database
-        response.status(200).json(results.rows[0]);//if rowcount is greater then zero it will execute 48 and send data from database
-      } else {
-        superagent.get(url)//Talking to Api
-          .then(data => {
-            const locationData = data.body[0];
-            const location = new Movie(city, locationData);
-            console.log('hi');
+  const url = `https://api.themoviedb.org/3/search/movie?api_key=${key}&query=${city}`;
 
-            let SQL = 'INSERT INTO cityexplorer (search_query, formatted_query,latitude,longitude ) VALUES ($1, $2, $3, $4)';// Referencing columns
-            let locValues = [location.search_query, location.formatted_query, location.latitude, location.longitude];
-            client.query(SQL, locValues)
-              .then(() => {
-                response.status(200).json(location);
-              })
-              .catch(error => {
-                console.log('ERROR', error);
-                response.status(500).send('So sorry, something went wrong.');
-              });
-          })
-          .catch(error => (console.log(error)));
-      }
+  superagent.get(url)//Talking to Api
+
+    .then(data => {
+      let movieInfo = data.body.results.map(vid => {
+        let baseUrl = 'https://image.tmdb.org/t/p/w300';
+        let filePath = data.body.poster_path;
+        let imageLink = baseUrl + filePath;
+
+        return new Movies(vid, imageLink);
+      });
+      response.status(200).send(movieInfo);
     })
-    .catch(error => {
-      console.log('ERROR', error);
-      response.status(500).send('So sorry, something went wrong.');
+    .catch(err => {
+      response.status(500).json('Good to go');
+      console.log(err);
     });
-}
 
+}
 function locationHandler(request, response) {//BUILD OUR REQUEST TO TALK TO LOCATIONIQ
   //Build request before we send it off to
   let city = request.query.city;
@@ -112,24 +98,25 @@ function locationHandler(request, response) {//BUILD OUR REQUEST TO TALK TO LOCA
     });
 }
 
-function yelpHandler(request, response) {
-  let key = process.env.YELP_API_KEY;
-  let lat = request.query.latitude;
-  let lon = request.query.longitude;
-  // console.log(lat);
-  const url = `https://api.yelp.com/v3/businesses/search`;
-  console.log(url);response
-  superagent.get(url)
-    .then(value => {
-      const yelpData = value.body.data.map(current => {
-        return new Weather(current);
-      });
-      response.status(200).send(yelpData);
-    }).catch(error => {
-      console.log('ERROR', error);
-      response.status(500).send('So sorry, something went wrong.');
-    });
-}
+// function yelpHandler(request, response) {
+//   let location = request.query.location;
+//   let key = process.env.YELP_API_KEY;
+//   let lat = request.query.latitude;
+//   let lon = request.query.longitude;
+//   // console.log(lat);
+//   const url = `https://api.yelp.com/v3/businesses/search?location=${location}`;
+//   console.log(url);response
+//   superagent.get(url)
+//     .then(value => {
+//       const yelpData = value.body.data.map(current => {
+//         return new Weather(current);
+//       });
+//       response.status(200).send(yelpData);
+//     }).catch(error => {
+//       console.log('ERROR', error);
+//       response.status(500).send('So sorry, something went wrong.');
+//     });
+// }
 
 function weatherHandler(request, response) {
   let key = process.env.WEATHER_API_KEY;
@@ -156,27 +143,30 @@ function Location(city, geoData) {
   this.longitude = geoData.lon;
 }
 
-//Yelp
+// Yelp
 // function Food(result){
-//   this.name = ;
-//   this.image_url = ;
-//   this.price = ;
-//   this.rating = ;
-//   this.url = ;
+//   this.name = result.name;
+//   this.image_url = result.image_url;
+//   this.price = result.price;
+//   this.rating = result.rating;
+//   this.url = result.url;
 // }
-
-//Movie
-function Movie(result) {
+function Weather(result) {
   this.time = new Date(result.ts * 1000).toDateString();
   this.forecast = result.weather.description;
+}
+
+
+//Movie
+function Movies(result) {
   // Based off movie object
-  this.title = result.title;
+  this.title = result.original_title;
   this.overview = result.overview;
-  this.average_votes = result.overview;
-  this.total_votes = result.total_votes;
-  this.image_url = result.image_url;
+  this.average_votes = result.vote_average;
+  this.total_votes = result.vote_count;
+  this.image_url = result.poster_path;
   this.popularity = result.popularity;
-  this.released_on = result.released_on;
+  this.released_on = result.release_date;
 }
 function notFoundHandler(request, response) {
   response.status(404).send('huh?');
@@ -194,4 +184,3 @@ client.connect()
   .catch(err => {
     console.log('ERROR', err);
   });
-
